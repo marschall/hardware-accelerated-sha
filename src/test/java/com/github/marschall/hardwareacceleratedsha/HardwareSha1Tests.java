b3fcfc86128281909ca45ad221bbdf84378c3054
Philippe Marschall <philippe.marschall@gmail.com>
2020-08-02 13:52:25 +0200

    bump unit tests

--- src/test/java/com/github/marschall/hardwareacceleratedsha/HardwareSha1Tests.java
+++ src/test/java/com/github/marschall/hardwareacceleratedsha/HardwareSha1Tests.java
@@ -1,7 +1,11 @@
 package com.github.marschall.hardwareacceleratedsha;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+import java.security.DigestException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.params.ParameterizedTest;
@@ -12,16 +16,62 @@ class HardwareSha1Tests {
   @ParameterizedTest
   @MethodSource("messageDigests")
   void singleByte(MessageDigest messageDigest) {
+    messageDigest.update((byte) 1);
+    byte[] digest = messageDigest.digest();
+    byte[] expected = new byte[] {-65, -117, 69, 48, -40, -46, 70, -35, 116, -84, 83, -95, 52, 113, -69, -95, 121, 65, -33, -9};
+    assertArrayEquals(expected, digest);
   }
 
   @ParameterizedTest
   @MethodSource("messageDigests")
-  void oneBlock(MessageDigest messageDigest) {
+  void targetDigest(MessageDigest messageDigest) throws DigestException {
+    messageDigest.update((byte) 1);
+    byte[] digest = new byte[22];
+    Arrays.fill(digest, (byte) 1);
+    messageDigest.digest(digest, 1, 20);
+    byte[] expected = new byte[] {1, -65, -117, 69, 48, -40, -46, 70, -35, 116, -84, 83, -95, 52, 113, -69, -95, 121, 65, -33, -9, 1};
+    assertArrayEquals(expected, digest);
+  }
+
+  @ParameterizedTest
+  @MethodSource("messageDigests")
+  void oneBlockNoOffset(MessageDigest messageDigest) {
+    byte[] message = new byte[64];
+    for (int i = 0; i < message.length; i++) {
+      message[i] = (byte) i;
+    }
+    messageDigest.update(message);
+    byte[] digest = messageDigest.digest();
+    byte[] expected = new byte[] {-58, 19, -115, 81, 79, -6, 33, 53, -65, -50, 14, -48, -72, -6, -58, 86, 105, -111, 126, -57};
+    assertArrayEquals(expected, digest);
+  }
+
+  @ParameterizedTest
+  @MethodSource("messageDigests")
+  void oneBlockWithOffset(MessageDigest messageDigest) {
+    byte[] message = new byte[66];
+    for (int i = 0; i < message.length; i++) {
+      message[i] = (byte) (i - 1);
+    }
+    messageDigest.update(message, 1, 64);
+    byte[] digest = messageDigest.digest();
+    byte[] expected = new byte[] {-58, 19, -115, 81, 79, -6, 33, 53, -65, -50, 14, -48, -72, -6, -58, 86, 105, -111, 126, -57};
+    assertArrayEquals(expected, digest);
   }
 
   @ParameterizedTest
   @MethodSource("messageDigests")
   void multipleBlocks(MessageDigest messageDigest) {
+    messageDigest.update((byte) 1);
+    byte[] message = new byte[65];
+    for (int i = 0; i < message.length; i++) {
+      message[i] = (byte) (i + 2);
+    }
+    messageDigest.update((byte) 1);
+    messageDigest.update(message);
+    byte[] digest = messageDigest.digest();
+    byte[] expected = new byte[] {29, -114, 67, 75, 8, 44, 109, -63, -107, -125, -111, 120, -52, 57, -29, -31, -50, -115, -37, -104};
+    assertArrayEquals(expected, digest);
   }
 
   static Stream<MessageDigest> messageDigests() throws NoSuchAlgorithmException {
